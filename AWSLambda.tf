799c44007386c857bc5a1ca5258eb115bb915e77
jackStudyCode <lzx493131129@gmail.com>
2022-09-22 16:49:11 -0700

    add lambda function role and policy

--- AWSLambda.tf
+++ AWSLambda.tf
@@ -12,20 +12,20 @@ data "archive_file" "init" {
 }
 
 ## S3 Bucket
-##resource "aws_s3_bucket" "function-bucket" {
-  ##bucket            = "lambda-function-bucket"
-##
-  ##tags = {
-    ##Name = "function-bucket-1"
-  ##}
-##}
+resource "aws_s3_bucket" "function-bucket" {
+  bucket = "lambda-function-bucket"
+
+  tags = {
+    Name = "function-bucket-1"
+  }
+}
 
 ## Upload zip file to s3 bucket
-##resource "aws_s3_object" "object" {
-  ##bucket = aws_s3_bucket.function-bucket.id
-  ##key = "hello.zip"
-  ##source = "hello.zip"
-##}
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.function-bucket.id
+  key = "hello.zip"
+  source = "hello.zip"
+}
 
 ## IAM role for lambda
 resource "aws_iam_role" "lambda_role" {
@@ -43,10 +43,10 @@ resource "aws_iam_role_policy" "lambda_policy" {
 
 ## AWS lambda functions
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "${local.lambda_zip_location}"
+  ##filename      = "${local.lambda_zip_location}"
   function_name = "hello"
-  ##s3_bucket     = aws_s3_bucket.function-bucket.id
-  ##s3_key        = "hello.zip"
+  s3_bucket     = aws_s3_bucket.function-bucket.id
+  s3_key        = "hello.zip"
   role          = "arn:aws:iam::137312912338:role/service-role/game-server-role"
   handler       = "hello.handler"
   ##source_code_hash = "${filebase64sha256(local.lambda_zip_location)}"
