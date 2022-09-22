a7e2f431c8a082c6a11682fd0530db96185007c1
jackStudyCode <lzx493131129@gmail.com>
2022-09-22 13:54:51 -0700

    add lambda function role and policy

--- AWSLambda.tf
+++ AWSLambda.tf
@@ -31,7 +31,7 @@ resource "aws_iam_role" "lambda_role" {
 ## IAM role-policy for lambda
 resource "aws_iam_role_policy" "lambda_policy" {
     name = "lambda_policy"
-    role = aws_iam_role.lambda_role_id
+    role = aws_iam_role.lambda_role.id
     policy = file("lambda_policy.json")
 }
 
