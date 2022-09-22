21ef31fc8f83896936195b7553deaabc0744ecb4
jackStudyCode <lzx493131129@gmail.com>
2022-09-22 12:51:23 -0700

    s3 typo fixed

--- AWSLambda.tf
+++ AWSLambda.tf
@@ -7,17 +7,17 @@ data "archive_file" "init" {
 }
 
 ## S3 Bucket
-resource "aws_s3_bucket" "function_bucket" {
-  bucket            = "lambda_function_bucket"
+resource "aws_s3_bucket" "function-bucket" {
+  bucket            = "lambda-function-bucket"
 
   tags = {
-    Name = "function_bucket-1"
+    Name = "function-bucket-1"
   }
 }
 
 ## Upload zip file to s3 bucket
 resource "aws_s3_object" "object" {
-  bucket = aws_s3_bucket.function_bucket.id
+  bucket = aws_s3_bucket.function-bucket.id
   key = "hello.zip"
   source = "${path.module}/hello.zip"
 }
@@ -25,7 +25,7 @@ resource "aws_s3_object" "object" {
 ## AWS lambda functions
 resource "aws_lambda_function" "test_lambda" {
   function_name = "hello"
-  s3_bucket     = aws_s3_bucket.function_bucket.id
+  s3_bucket     = aws_s3_bucket.function-bucket.id
   s3_key        = "hello.zip"
   role          = "arn:aws:iam::137312912338:role/service-role/game-server-role"
   handler       = "hello.handler"
