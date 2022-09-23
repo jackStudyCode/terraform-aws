a0d1ee99e6d471b5ac04dd3df3df9829ed1c09f2
jackStudyCode <lzx493131129@gmail.com>
2022-09-23 12:23:33 -0700

    add lambda function for game

--- AWSLambda_JoinGame.tf
+++ AWSLambda_JoinGame.tf
@@ -1,11 +1,11 @@
 
 locals {
-    lambda_zip_location = "${path.module}/join-game-1.zip"
+    lambda_join_game_zip_location = "${path.module}/join-game-1.zip"
 }
 
 ##Zip the function to be run at function App.
 
-data "archive_file" "init" {
+data "archive_file" "init_join_game" {
   type        = "zip"
   source_file = "${path.module}/join-game-1.js"
   output_path = "${path.module}/join-game-1.zip"
@@ -13,15 +13,15 @@ data "archive_file" "init" {
 
 
 ## AWS lambda functions
-resource "aws_lambda_function" "test_lambda" {
-  filename      = "${local.lambda_zip_location}"
+resource "aws_lambda_function" "join_game_lambda" {
+  filename      = "${local.lambda_join_game_zip_location}"
   function_name = "join-game-1"
   ##s3_bucket     = aws_s3_bucket.function-bucket.id
   ##s3_key        = "join-game-1.zip"
   role          = "arn:aws:iam::137312912338:role/service-role/game-server-role"
   handler       = "join-game-1.handler"
-  ##source_code_hash = "${filebase64sha256(local.lambda_zip_location)}"
+  ##source_code_hash = "${filebase64sha256(local.lambda_join_game_zip_location)}"
   ##source_code_hash = data.aws_s3_bucket_object.hash.body
-  source_code_hash = "${data.archive_file.init.output_base64sha256}"
+  source_code_hash = "${data.archive_file.init_join_game.output_base64sha256}"
   runtime       = "nodejs16.x"
 }
