bd462ce857552265d814898c497378abbd0cbad9
jackStudyCode <lzx493131129@gmail.com>
2022-09-23 18:25:20 -0700

    fmt terraform files

--- AWSLambda.tf
+++ AWSLambda.tf
@@ -1,6 +1,6 @@
 
 locals {
-    lambda_zip_location = "${path.module}/hello.zip"
+  lambda_zip_location = "${path.module}/hello.zip"
 }
 
 ##Zip the function to be run at function App.
@@ -23,39 +23,39 @@ resource "aws_s3_bucket" "function-bucket" {
 ## Upload zip file to s3 bucket
 resource "aws_s3_object" "object" {
   bucket = aws_s3_bucket.function-bucket.id
-  key = "hello.zip"
+  key    = "hello.zip"
   source = "${path.module}/hello.zip"
 }
 
 ## IAM role for lambda
 resource "aws_iam_role" "lambda_role" {
-    name = "lambda_role"
-    assume_role_policy = file("lambda_assume_role_policy.json")
+  name               = "lambda_role"
+  assume_role_policy = file("lambda_assume_role_policy.json")
 }
 
 ## IAM role-policy for lambda
 resource "aws_iam_role_policy" "lambda_policy" {
-    name = "lambda_policy"
-    role = aws_iam_role.lambda_role.id
-    policy = file("lambda_policy.json")
+  name   = "lambda_policy"
+  role   = aws_iam_role.lambda_role.id
+  policy = file("lambda_policy.json")
 }
 
 
 ##data "aws_s3_bucket_object" "hash" {
-  ##bucket = aws_s3_bucket.function-bucket.id
-  ##key    = ${"hello.zip"}
+##bucket = aws_s3_bucket.function-bucket.id
+##key    = ${"hello.zip"}
 ##}
 
 ## AWS lambda functions
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "${local.lambda_zip_location}"
+  filename      = local.lambda_zip_location
   function_name = "hello"
   ##s3_bucket     = aws_s3_bucket.function-bucket.id
   ##s3_key        = "hello.zip"
-  role          = "arn:aws:iam::137312912338:role/service-role/game-server-role"
-  handler       = "hello.handler"
+  role    = "arn:aws:iam::137312912338:role/service-role/game-server-role"
+  handler = "hello.handler"
   ##source_code_hash = "${filebase64sha256(local.lambda_zip_location)}"
   ##source_code_hash = data.aws_s3_bucket_object.hash.body
-  source_code_hash = "${data.archive_file.init.output_base64sha256}"
-  runtime       = "nodejs16.x"
+  source_code_hash = data.archive_file.init.output_base64sha256
+  runtime          = "nodejs16.x"
 }
--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 terraform {
-    #Terraform version at the time of writing this post
-    required_version = ">= 0.12.24"
+  #Terraform version at the time of writing this post
+  required_version = ">= 0.12.24"
 
-    backend "s3"{
-        bucket = "github-terraform-aws-tfstates"
-        key = "infra.tfstate"
-        region = "us-west-1"
-    }
+  backend "s3" {
+    bucket = "github-terraform-aws-tfstates"
+    key    = "infra.tfstate"
+    region = "us-west-1"
+  }
 }
 
 provider "random" {}
 
 ## Provider us-west-1
 provider "aws" {
-    region = "us-west-1"
+  region = "us-west-1"
 }
